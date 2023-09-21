305abf4437f07bc88a4c7874339c6aedec4147e2
Sukumar2022 <s.mondal24.11.2000@gmail.com>
2023-09-21 19:12:54 +0530

    minor
    minor project

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculatePercentage() {
+    let currentValue = parseFloat(display.value);
+    display.value = (currentValue - (currentValue * 0.01)).toString();
+}
+
+function calculateSquare() {
+    let currentValue = parseFloat(display.value);
+    display.value = (currentValue * currentValue).toString();
+}
+
+
+
+
